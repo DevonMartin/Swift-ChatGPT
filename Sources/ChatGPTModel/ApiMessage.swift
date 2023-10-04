956c575e0f1f533b20bb61d565649397978977b2
Devon Martin <devon.martin.re@gmail.com>
2023-10-04 18:41:07 -0400

    Updated access to messages to provide a consistent form a transportation for messages.

--- Sources/ChatGPTModel/ApiMessage.swift
+++ Sources/ChatGPTModel/ApiMessage.swift
@@ -6,15 +6,15 @@
 //
 
 public struct OpenAiApiMessage: Codable {
-	let role: String
-	let content: String
+	public let role: String
+	public let content: String
 	
-	init(role: Role, content: String) {
+	public init(role: Role, content: String) {
 		self.role = role.rawValue
 		self.content = content
 	}
 	
-	enum Role: String {
+	public enum Role: String {
 		case system = "system"
 		case user = "user"
 		case assistant = "assistant"
